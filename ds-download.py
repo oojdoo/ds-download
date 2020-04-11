8d0f862abcd49515dd60b718761e4b22ea3440ca
oojdoo <oojdoo@laposte.net>
2020-04-11 16:54:38 +0200

    ajout du mode d'emploi du programme v2

--- ds-download.py
+++ ds-download.py
@@ -34,8 +34,8 @@ token = "zezezezezezzezezezezeze"      # laissez les guillemets
 premier_intitule_nom_piece_jointe = "Nom de l'élève"        # laissez les guillemets
 deuxieme_intitule_nom_piece_jointe = "Prénom de l'élève"    # laissez les guillemets
 
-# mode d'emploi : placez le programme ds-download.py dans un nouveau dossier,
-# ouvrez un terminal dans ce dossier, lancez le programme avec la commande
+# mode d'emploi sous linux : placez le programme ds-download.py dans un nouveau 
+# dossier, ouvrez un terminal dans ce dossier, lancez le programme avec la commande
 # suivante    python3 ds-download.py
 
 #########################################################
