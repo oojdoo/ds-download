284233cd08321501eb15a007261e7369728c7567
oojdoo <oojdoo@laposte.net>
2020-04-20 19:19:50 +0200

    Dimensions de fenêtre sont fixes.

--- ds_download_gui.py
+++ ds_download_gui.py
@@ -2,17 +2,19 @@
 #  Programme sous licence creative commons CC0 1.0 Public Domain  #
 #  https://creativecommons.org/publicdomain/zero/1.0/deed.fr      #
 ###################################################################
-
-import tkinter as tk
+try:
+    import tkinter as tk
+except:
+    import Tkinter as tk
 import ds_download
 
 def build_frame(titre, window, liste):
     def recupere_proc():
         if ds_download.lancement(liste_entries[0].get(), liste_entries[1].get()):
-            text.set("Terminé! Les pièces jointes se trouvent dans le dossier pieces_jointes.")
+            text.set('Les pièces jointes se trouvent dans le dossier pieces_jointes.')
             etat.config(bg="green", fg="white") 
         else:
-            text.set("        Le téléchargement a échoué. Vérifiez les deux champs.      ")
+            text.set("Le téléchargement a échoué.Vérifiez les deux champs ci-dessus.")
             etat.config(bg="red", fg="white")
     frame = tk.LabelFrame(window, text=titre, padx = 20, pady = 20)
     liste_labels = [tk.Label(frame, text=e + ' :') for e in liste]
@@ -32,6 +34,8 @@ def build_frame(titre, window, liste):
 if __name__ == '__main__': 
     fenetre = tk.Tk()
     fenetre.title('ds_download_gui')
+    fenetre.geometry('468x158')
+    fenetre.resizable(width=False, height=False)
     frame = build_frame("Configuration de la procédure",
                         fenetre, ["Numéro de procédure", "Token"])
     fenetre.mainloop()
